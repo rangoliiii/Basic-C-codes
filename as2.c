ec2c064fb58a217447278d5b506ee3dd41b8f8a4
rangoliiii <69386931+rangoliiii@users.noreply.github.com>
2021-09-06 23:15:51 +0530

    Add files via upload
    Minimum of three numbers

---
+++ as2.c
@@ -0,0 +1,35 @@
+/*Write a program to find
+ minimum of three numbers
+
+ Name: Rangoli Dhawan
+ Roll number: 53
+ Section: B
+ */
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("\t\t\t\t*****INPUT*****\n");
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+    printf("Enter third number:");
+    scanf("%d",&c);
+    printf("\t\t\t\t*****OUTPUT*****\n");
+    if(a<b && a<c)
+    {
+        printf("Minmum among %d,%d,%d is:%d",a,b,c,a);
+    }
+    else if(b<a && b<c)
+    {
+        printf("Minmum among %d,%d,%d is:%d",a,b,c,b);
+    }
+    else
+    {
+        printf("Minmum among %d,%d,%d is:%d",a,b,c,c);
+    }
+    getch();
+}
